616d38c93be2fd12807768d89ce5a21bba3ab1d1
CHOI GIHOAWN <rlghks3004@gmail.com>
2023-06-22 23:08:30 +0900

    회원가입 API 작성 완료

---
+++ src/app/api/auth/signup/handler.ts
@@ -0,0 +1,50 @@
+import { connectToDatabase } from "@/lib/db";
+import { WithId } from "mongodb";
+import { NextResponse } from "next/server";
+import { hashPassword } from "@/lib/auth";
+
+interface User extends WithId<Document> {
+  email: string;
+  password: string;
+}
+
+const createUserHandler = async ({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) => {
+  const { DB_NAME, COLLECTION_NAME } = process.env;
+  if (!DB_NAME || !COLLECTION_NAME) {
+    throw new Error("Can not found database");
+  }
+
+  const client = await connectToDatabase();
+  const collection = await client.db(DB_NAME).collection(COLLECTION_NAME);
+
+  if (!collection) {
+    await client.close();
+    throw new Error("Can not found collection");
+  }
+
+  const user = (await collection.findOne({ email: email })) as User | null;
+  if (user) {
+    await client.close();
+    return NextResponse.json({
+      message: "There are already user",
+      isValid: false,
+    });
+  }
+
+  const hashedPassword = await hashPassword(password);
+
+  const pushResult = await collection.insertOne({
+    email: email,
+    password: hashedPassword,
+  });
+
+  return pushResult.acknowledged;
+};
+
+export default createUserHandler;
---
+++ src/app/api/auth/signup/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+import handler from "@/app/api/auth/signup/handler";
+
+interface SignUpUser {
+  email: string;
+  password: string;
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    const data: SignUpUser = await req.json();
+
+    const isInserted = await handler(data);
+
+    if (isInserted) {
+      return NextResponse.json({
+        message: "Create account successfully!",
+        isValid: true,
+      });
+    } else {
+      return NextResponse.json({
+        message: "Failed to create account. Try again!",
+        isValid: false,
+      });
+    }
+  } catch (error) {
+    throw new Error("Something wrong!");
+  }
+}
